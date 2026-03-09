03e6abc0f43e0b7ebe321d1f54797550e3ca55e5
leibt <leila.benloukar@hotmail.com>
2026-03-09 12:23:11 +0100

    Update HelloWorld.java

--- backend/src/HelloWorld.java
+++ backend/src/HelloWorld.java
@@ -1,9 +1,6 @@
 public class HelloWorld {
-    
-    // Your program begins with a call to main()
-    public static void main(String[] args)
-    {
-        // Prints "Hello, World" to the terminal window.
+        public static void main(String[] args)
+    {        
         System.out.println("Hello, World");
     }
 }
